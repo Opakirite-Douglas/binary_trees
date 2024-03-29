ea1679e26258763c240c61d3b9638aa35c713359
Opakirite-Douglas <ddebbyd82@gmail.com>
2024-03-29 13:36:59 -0700

    Mandatory all done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance - To measure the balance factor of a binary tree
+ * binary_tree_balance - This measures the balance
+ * factor of a binary tree
  *
  * @tree: Just a pointer
  *
@@ -11,14 +12,14 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (tree)
-		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+        if (tree)
+                return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-	return (0);
+        return (0);
 }
 
 /**
- * binary_tree_height - Measures height of a binary tree
+ * binary_tree_height - This measure the height of a binary tree
  *
  * @tree: Just a pointer
  *
@@ -28,12 +29,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, mhay = 0;
+        if (tree)
+        {
+                size_t l = 0, jhay = 0;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-		mhay = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-		return ((l > mhay) ? l : mhay);
-	return (0);
+                l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+                jhay = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+                return ((l > jhay) ? l : jhay);
+        }
+        return (0);
 }
