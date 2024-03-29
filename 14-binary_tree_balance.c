4ae1a7a44e63edd3d337c316e5dba9a8fbc700f4
Opakirite-Douglas <ddebbyd82@gmail.com>
2024-03-29 13:45:00 -0700

    Mandatory all done

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,10 +12,10 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-        if (tree)
-                return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
+	if (tree)
+		return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 
-        return (0);
+	return (0);
 }
 
 /**
@@ -29,13 +29,13 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        if (tree)
-        {
-                size_t l = 0, jhay = 0;
+	if (tree)
+	{
+		size_t l = 0, mhay = 0;
 
-                l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
-                jhay = tree->right ? 1 + binary_tree_height(tree->right) : 1;
-                return ((l > jhay) ? l : jhay);
-        }
-        return (0);
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		mhay = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > mhay) ? l : mhay);
+	}
+	return (0);
 }
